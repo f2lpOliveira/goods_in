a62a19ab63c26622a6d8aa6af071219d78ea4bf2
f2lpOliveira <contato@fellipefreire.com.br>
2026-07-18 17:50:38 +0100

    feat: render home view

---
+++ app/js/app.js
@@ -0,0 +1,3 @@
+import { renderHome } from "./views/home.js";
+
+renderHome();
--- app/js/views/home.js
+++ app/js/views/home.js
@@ -1,1 +1,27 @@
-export function renderHome() {}
+export function renderHome() {
+  const appContent = document.getElementById("app-content");
+
+  appContent.innerHTML = `
+			<section class="home-view">
+
+					<button id="new-record-button">
+							New Record
+					</button>
+
+					<p>No records found.</p>
+
+					<div class="actions">
+
+							<button>
+									Export
+							</button>
+
+							<button>
+									Settings
+							</button>
+
+					</div>
+
+			</section>
+	`;
+}
