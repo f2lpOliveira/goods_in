024c718a6ebe1b3eecced0adaff9177310f834bd
f2lpOliveira <contato@fellipefreire.com.br>
2026-07-24 22:32:10 +0100

    chore: improve service worker cache versioning

--- app/service-worker.js
+++ app/service-worker.js
@@ -60,3 +60,15 @@ self.addEventListener("fetch", event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        )
+      )
+  );
+});
