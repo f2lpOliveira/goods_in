5cf193c12ffd73e74685a1ba7150db87b11a3412
f2lpOliveira <contato@fellipefreire.com.br>
2026-07-28 20:54:56 +0100

    feat: add barcode parser factory

--- app/js/core/barcode/parserFactory.js
+++ app/js/core/barcode/parserFactory.js
@@ -1,3 +1,18 @@
+import { detectBarcodeFormat } from "./detectBarcodeFormat.js";
+import { BARCODE_TYPES } from "./barcodeTypes.js";
+import { parseGS1 } from "./parsers/gs1Parser.js";
+
 export function parseBarcode(barcode) {
-  return null;
+  const barcodeType = detectBarcodeFormat(barcode);
+
+  switch (barcodeType) {
+    case BARCODE_TYPES.GS1:
+      return parseGS1(barcode);
+
+    default:
+      return {
+        type: BARCODE_TYPES.UNKNOWN,
+        raw: barcode,
+      };
+  }
 }
--- app/js/core/barcode/parsers/gs1Parser.js
+++ app/js/core/barcode/parsers/gs1Parser.js
@@ -1,3 +1,8 @@
+import { BARCODE_TYPES } from "../barcodeTypes.js";
+
 export function parseGS1(barcode) {
-  return null;
+  return {
+    type: BARCODE_TYPES.GS1,
+    raw: barcode,
+  };
 }
