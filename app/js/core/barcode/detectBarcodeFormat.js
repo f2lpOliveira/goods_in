dc30825a309431a98551c53e2779a22e876a88d6
f2lpOliveira <contato@fellipefreire.com.br>
2026-07-28 20:35:54 +0100

    feat: detect gs1 barcode format

--- app/js/core/barcode/detectBarcodeFormat.js
+++ app/js/core/barcode/detectBarcodeFormat.js
@@ -1,3 +1,15 @@
+import { BARCODE_TYPES } from "./barcodeTypes.js";
+
 export function detectBarcodeFormat(barcode) {
-  return null;
+  if (!barcode) {
+    return BARCODE_TYPES.UNKNOWN;
+  }
+
+  const normalizedBarcode = barcode.trim();
+
+  if (normalizedBarcode.startsWith("02")) {
+    return BARCODE_TYPES.GS1;
+  }
+
+  return BARCODE_TYPES.UNKNOWN;
 }
